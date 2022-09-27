c0acbf0642622078646e004e1104fdcb9848638b
Shakib Sadat Shanto <62327880+shakib-sadat@users.noreply.github.com>
2022-09-27 19:13:13 +0600

    Add files via upload

---
+++ Lab/Lab1.R
@@ -0,0 +1,41 @@
+x<-'Run R'
+tolower(x)
+strsplit(x,split = " ")
+nchar(x)
+y<-'and die'
+paste(x,y)
+
+# a<-nchar(x)
+# b<-nchar(y)
+
+# sum<-function()
+# {
+# if(a>b)
+# {
+#     print("a is greater than b")
+# }else {
+#   print("b is greater than a")
+# }
+
+# }
+
+# sum()
+
+# p =readline(prompt="Enter a word: ")
+
+# x<-6
+# if(x%%2==0)
+# {
+#     print("x is even")
+# }else{
+#     print("x is odd")
+# }
+
+# x<-c(110,25,100,900)
+# y<-c("A","B","C","D")
+# min(x)
+# max(x)
+
+# barplot(x, names.arg=y, col="purple", density = 10)
+# pie(x)
+# plot(x,type="l", col="green", lwd=5)
